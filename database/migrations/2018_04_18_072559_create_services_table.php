646c006fe239b4e1ec0910adb94408e695841271
DevKing <wow.webgenius@gmail.com>
2018-04-18 14:35:03 +0700

    table added for services

---
+++ database/migrations/2018_04_18_072559_create_services_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned();
+            $table->string('name');
+            $table->integer('price');
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropForeign('services_company_id_foreign');
+        });
+        Schema::dropIfExists('services');
+    }
+}
