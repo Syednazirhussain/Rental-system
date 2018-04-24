07557e13bd19dad69cfc7857ad348ed03e1f9896
DevKing <wow.webgenius@gmail.com>
2018-04-25 00:57:42 +0700

    remove contract show button

--- resources/views/company/contracts/table.blade.php
+++ resources/views/company/contracts/table.blade.php
@@ -20,7 +20,6 @@
             <td  width="200px" class="text-center">
                 {!! Form::open(['route' => ['company.contracts.destroy', $contract->id], 'method' => 'delete']) !!}
                 <a href="{{ route('company.generateInvoice', ['contract_id' => $contract->id]) }}" class="btn btn-primary btn-xs">Generate Invoice&nbsp;<i class="fa fa-arrow-right fa-md"></i></a>
-                <a href="{!! route('company.contracts.show', [$contract->id]) !!}"><i class="fa fa-eye fa-lg text-info"></i></a>
                 <a href="{!! route('company.contracts.edit', [$contract->id]) !!}"><i class="fa fa-edit fa-lg text-info"></i></a>
                 {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 {!! Form::close() !!}
