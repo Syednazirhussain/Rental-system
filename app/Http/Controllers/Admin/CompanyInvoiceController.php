23afe5cd1e37ccc30fde30651666ded546de879e
Faizan Afzal <thefaizan@gmail.com>
2018-04-26 21:24:49 +0500

    Fix

--- app/Http/Controllers/Admin/CompanyInvoiceController.php
+++ app/Http/Controllers/Admin/CompanyInvoiceController.php
@@ -163,7 +163,7 @@ class CompanyInvoiceController extends AppBaseController
     {
         $lastInvoice =  $this->companyInvoiceRepository->getLastInsertedInvoiceId();
         $Invoice_id =  $lastInvoice->id;      
-        $filename = $Invoice_id."_Invoices.pdf";         
+        $filename = "invoice_".$Invoice_id.".pdf";         
         $filePath = public_path().DIRECTORY_SEPARATOR."storage".DIRECTORY_SEPARATOR."company_invoices".DIRECTORY_SEPARATOR.$filename;
         $data = ['Path' => $filePath];
         $company_infomation = $this->getCompanyDetailById($company_id); 
@@ -179,7 +179,7 @@ class CompanyInvoiceController extends AppBaseController
     public function sendInvoiceToCompanyContactPersonByInvoiceId($company_id,$invoice_id)
     {
         $Invoice_id =  $invoice_id;      
-        $filename = $Invoice_id."_Invoices.pdf";         
+        $filename = "invoice_".$Invoice_id.".pdf";         
         $filePath = public_path().DIRECTORY_SEPARATOR."storage".DIRECTORY_SEPARATOR."company_invoices".DIRECTORY_SEPARATOR.$filename;
         $data = ['Path' => $filePath];
         $company_infomation = $this->getCompanyDetailById($company_id); 
