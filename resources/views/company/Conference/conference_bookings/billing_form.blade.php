41c150a8cf197bf187583a295407a20e033d1712
murehman <moizrehman1994@gmail.com>
2018-06-12 13:07:06 +0500

    booking conference customer form working

--- resources/views/company/Conference/conference_bookings/billing_form.blade.php
+++ resources/views/company/Conference/conference_bookings/billing_form.blade.php
@@ -3,8 +3,10 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Invoice Send as</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
-                    <option value=""></option>
+                <select class="form-control select2-invoice_send" id="invoice_send" name="invoice_send">
+                    <option></option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->invoice_send_as == "email" ) { echo "selected"; } ?> value="email">Email</option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->invoice_send_as == "print" ) { echo "selected"; } ?> value="print">Print</option>
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -13,7 +15,7 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Reference</label>
-                <input class="form-control" id="customer_address" name="customer_address">
+                <input class="form-control" id="reference" name="reference" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->reference}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
@@ -21,8 +23,11 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Contact Person</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
+                <select class="form-control select2-contact_person" id="contact_person" name="contact_person">
                     <option value=""></option>
+                    @foreach($getCompanyCustomer as $companyCustomer)
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->contact_person == $companyCustomer->user_id ) { echo "selected"; } ?> value="{{$companyCustomer->user_id}}">{{$companyCustomer->user->name}}</option>
+                    @endforeach
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -31,7 +36,7 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Cost</label>
-                <input class="form-control" id="customer_address" name="customer_address">
+                <input class="form-control" id="cost" name="cost" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->cost}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
@@ -39,8 +44,11 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Payment Conditions</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
-                    <option value=""></option>
+                <select class="form-control select2-payment_conditions" id="payment_conditions" name="payment_conditions">
+                    <option></option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->payment_condition == "qwe" ) { echo "selected"; } ?> value="qwe">qwe</option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->payment_condition == "asd" ) { echo "selected"; } ?>  value="asd">asd</option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->payment_condition == "zxc" ) { echo "selected"; } ?>  value="zxc">zxc</option>
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -49,8 +57,10 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Interest Fees</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
+                <select class="form-control select2-interest_fees" id="interest_fees" name="interest_fees">
                     <option value=""></option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->interest_fees == "yes" ) { echo "selected"; } ?>  value="yes">Yes</option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->interest_fees == "no" ) { echo "selected"; } ?>  value="no">No</option>
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -59,8 +69,10 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Payment Reminder</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
+                <select class="form-control select2-payment_reminder" id="payment_reminder" name="payment_reminder">
                     <option value=""></option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->peyment_reminder == "yes" ) { echo "selected"; } ?>  value="yes">Yes</option>
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->peyment_reminder == "no" ) { echo "selected"; } ?>  value="no">No</option>
                 </select>
                 <div class="errorTxt"></div>
             </div>
--- resources/views/company/Conference/conference_bookings/customer_form.blade.php
+++ resources/views/company/Conference/conference_bookings/customer_form.blade.php
@@ -3,8 +3,11 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Customer</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
+                <select class="form-control select2-customer" id="customer_id" name="customer_id">
                     <option value=""></option>
+                    @foreach($getCompanyCustomer as $companyCustomer)
+                    <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->user_id == $companyCustomer->user_id ) { echo "selected"; } ?> value="{{$companyCustomer->user_id}}">{{$companyCustomer->user->name}}</option>
+                    @endforeach
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -13,7 +16,7 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Address</label>
-                <input class="form-control" id="customer_address" name="customer_address">
+                <input class="form-control" id="customer_address" name="customer_address" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->address}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
@@ -21,8 +24,17 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Country</label>
-                <select class="form-control select2-customer" id="country" name="customer">
-                    <option value=""></option>
+                <select class="form-control select2-country" id="customer_country" name="customer_country">
+                    <option></option>
+                    @foreach ($countries as $country)
+                         @if (isset($company) && $country->short == $company->country->short)
+                          <option value="{{ $country->id }}" @if(isset($companyCustomerInfo) && $country->id == $companyCustomerInfo->$country_id )  @endif >{{ $country->name }}</option> 
+                         @elseif ($country->short == 'SE')
+                          <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->country_id == $country->id ) { echo "selected"; } ?> value="{{ $country->id }}" >{{ $country->name }}</option> 
+                         @else
+                          <option value="{{ $country->id }}" >{{ $country->name }}</option> 
+                         @endif
+                    @endforeach
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -31,8 +43,15 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">State</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
-                    <option value=""></option>
+                <select class="form-control select2-state" id="customer_state" name="customer_state">
+                    <option></option>
+                    @foreach ($states as $state)
+                         @if (isset($company) && $state->id == $company->state->id)
+                          <option value="{{ $state->id }}" selected="selected">{{ $state->name }}</option> 
+                         @else
+                          <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->state_id == $state->id ) { echo "selected"; } ?> value="{{ $state->id  }}">{{ $state->name }}</option> 
+                         @endif
+                    @endforeach
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -41,8 +60,15 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">City</label>
-                <select class="form-control select2-customer" id="customer" name="customer">
-                    <option value=""></option>
+                <select class="form-control select2-city" id="customer_city" name="customer_city">
+                    <option></option>
+                    @foreach ($cities as $city)
+                      @if (isset($company) && $city->id == $company->city->id)
+                       <option value="{{ $city->id }}" selected="selected">{{ $city->name }}</option> 
+                      @else
+                       <option <?php if(isset($companyCustomerInfo) && $companyCustomerInfo->city_id == $city->id ) { echo "selected"; } ?> value="{{ $city->id  }}">{{ $city->name }}</option> 
+                      @endif
+                    @endforeach
                 </select>
                 <div class="errorTxt"></div>
             </div>
@@ -50,8 +76,8 @@
 
 	<div class="col-md-6">
             <div class="form-group m-t-2">
-                <label for="customer">Email</label>
-                <input class="form-control" id="post_code" name="post_code">
+                <label for="customer">Postal Code</label>
+                <input class="form-control" id="customer_post_code" name="customer_post_code" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->postal_code}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
@@ -59,7 +85,7 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Telephone</label>
-                <input class="form-control" id="post_code" name="post_code">
+                <input class="form-control" id="customer_telephone" name="customer_telephone" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->telephone}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
@@ -67,7 +93,7 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Mobile</label>
-                <input class="form-control" id="post_code" name="post_code">
+                <input class="form-control" id="customer_mobile" name="customer_mobile" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->mobile}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
@@ -75,7 +101,7 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Fax</label>
-                <input class="form-control" id="post_code" name="post_code">
+                <input class="form-control" id="customer_fax" name="customer_fax" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->fax}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
@@ -83,7 +109,7 @@
 	<div class="col-md-6">
             <div class="form-group m-t-2">
                 <label for="customer">Organization No.</label>
-                <input class="form-control" id="post_code" name="post_code">
+                <input class="form-control" id="customer_org_num" name="customer_org_num" value="@if(isset($companyCustomerInfo)){{$companyCustomerInfo->organization_num}}@endif">
                 <div class="errorTxt"></div>
             </div>
 	</div>
